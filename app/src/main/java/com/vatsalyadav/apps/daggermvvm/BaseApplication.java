099b0d996f7d5e20f57f9a71688e9e2dd6c2b923
Vatsal Yadav <vatsalyad15@gmail.com>
2020-05-11 02:57:43 +0530

    Setup AppComponent and BaseApplication

---
+++ app/src/main/java/com/vatsalyadav/apps/daggermvvm/BaseApplication.java
@@ -0,0 +1,13 @@
+package com.vatsalyadav.apps.daggermvvm;
+
+import com.vatsalyadav.apps.daggermvvm.di.DaggerAppComponent;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+
+public class BaseApplication extends DaggerApplication {
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().application(this).build(); // Binding an application instance to application component
+    }
+}
---
+++ app/src/main/java/com/vatsalyadav/apps/daggermvvm/di/AppComponent.java
@@ -0,0 +1,29 @@
+package com.vatsalyadav.apps.daggermvvm.di;
+
+import android.app.Application;
+
+import com.vatsalyadav.apps.daggermvvm.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(
+        modules = {
+                AndroidSupportInjectionModule.class
+        }
+)
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+            // override Builder method
+    interface Builder {
+
+        @BindsInstance
+            // if you want to bind a particular object through the instance at the time of its construction
+        Builder application(Application application);
+
+        AppComponent build(); // mandatory step
+    }
+}
