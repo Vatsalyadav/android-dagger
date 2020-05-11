02cd1ff8b465fff10b77cc3a59aa8f8cd0eb4a90
Vatsal Yadav <vatsalyad15@gmail.com>
2020-05-12 04:46:52 +0530

    Wrap up Retrofit auth response with AuthResource and set status

--- app/src/main/java/com/vatsalyadav/apps/daggermvvm/AuthActivity.java
+++ app/src/main/java/com/vatsalyadav/apps/daggermvvm/AuthActivity.java
@@ -7,9 +7,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.bumptech.glide.RequestManager;
 import com.vatsalyadav.apps.daggermvvm.model.User;
+import com.vatsalyadav.apps.daggermvvm.ui.auth.AuthResource;
 import com.vatsalyadav.apps.daggermvvm.ui.auth.AuthViewModel;
 import com.vatsalyadav.apps.daggermvvm.viewmodels.ViewModelProviderFactory;
 
@@ -19,13 +22,14 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import dagger.android.support.DaggerAppCompatActivity;
 
-public class AuthActivity extends DaggerAppCompatActivity implements View.OnClickListener{
+public class AuthActivity extends DaggerAppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "AuthActivity";
     @Inject
     ViewModelProviderFactory providerFactory;
     private AuthViewModel viewModel;
     private EditText userId;
+    private ProgressBar progressBar;
     @Inject
     Drawable logo;
 
@@ -37,6 +41,7 @@ public class AuthActivity extends DaggerAppCompatActivity implements View.OnClic
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
         userId = findViewById(R.id.user_id_input);
+        progressBar = findViewById(R.id.progress_bar);
         findViewById(R.id.login_button).setOnClickListener(this);
 
         viewModel = new ViewModelProvider(this, providerFactory).get(AuthViewModel.class);
@@ -54,28 +59,48 @@ public class AuthActivity extends DaggerAppCompatActivity implements View.OnClic
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.login_button:
                 attemptLogin();
                 break;
         }
     }
 
-    private void attemptLogin(){
-        if(TextUtils.isEmpty(userId.getText().toString())){
+    private void attemptLogin() {
+        if (TextUtils.isEmpty(userId.getText().toString())) {
             return;
         }
         viewModel.authenticateWithId(Integer.parseInt(userId.getText().toString()));
     }
 
-    private void subscribeObserver(){
-        viewModel.observeUser().observe(this, new Observer<User>() {
+    private void subscribeObserver() {
+        viewModel.observeUser().observe(this, new Observer<AuthResource<User>>() {
             @Override
-            public void onChanged(User user) {
-                if(user != null){
-                    Log.d(TAG, "onChanged: "+user.getEmail());
+            public void onChanged(AuthResource<User> userAuthResource) {
+                if (userAuthResource != null) {
+                    switch (userAuthResource.status){
+                        case LOADING:
+                            showProgressBar(true);
+                            break;
+                        case ERROR:
+                            showProgressBar(false);
+                            Toast.makeText(AuthActivity.this, "Failed to fetch",Toast.LENGTH_LONG).show();
+                            break;
+                        case AUTHENTICATED:
+                            showProgressBar(false);
+                            Toast.makeText(AuthActivity.this, "Login Success",Toast.LENGTH_LONG).show();
+                            Log.d(TAG, "onChanged: Login Success"+userAuthResource.data.getEmail());
+                            break;
+                        case NOT_AUTHENTICATED:
+                            showProgressBar(false);
+                            Toast.makeText(AuthActivity.this, "Not Authenticated",Toast.LENGTH_LONG).show();
+                            break;
+                    }
                 }
             }
         });
+    }
+    private void showProgressBar(boolean isVisible){
+        progressBar.setVisibility(isVisible?View.VISIBLE:View.GONE);
     }
 }
--- app/src/main/java/com/vatsalyadav/apps/daggermvvm/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/vatsalyadav/apps/daggermvvm/ui/auth/AuthViewModel.java
@@ -12,13 +12,14 @@ import androidx.lifecycle.LiveDataReactiveStreams;
 import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel {
 
     private final AuthApi authApi;
 
-    private MediatorLiveData<User> authUser = new MediatorLiveData<>();
+    private MediatorLiveData<AuthResource<User>> authUser = new MediatorLiveData<>();
 
     @Inject
     public AuthViewModel(AuthApi authApi) { // Requirement for multi binding
@@ -27,21 +28,39 @@ public class AuthViewModel extends ViewModel {
     }
 
     public void authenticateWithId(int userId){
-        final LiveData<User> source = LiveDataReactiveStreams.fromPublisher(
+        authUser.setValue(AuthResource.loading((User)null)); // will trigger the progress bar
+        final LiveData<AuthResource<User>> source = LiveDataReactiveStreams.fromPublisher(
                 authApi.getUser(userId)
+                        .onErrorReturn(new Function<Throwable, User>() {
+                            @Override
+                            public User apply(Throwable throwable) throws Exception {
+                                User errorUser = new User();
+                                errorUser.setId(-1);
+                                return errorUser;
+                            }
+                        })
+                        .map(new Function<User, AuthResource<User>>() {
+                            @Override
+                            public AuthResource<User> apply(User user) throws Exception {
+                                if(user.getId() == -1){
+                                    return AuthResource.error("Could not authenticate", (User)null);
+                                }
+                                return AuthResource.authenticated(user);
+                            }
+                        })
                 .subscribeOn(Schedulers.io())
         );
-        // Now how to convert the source to the authUser? That's why we are using MediatorLiveData
-        authUser.addSource(source, new Observer<User>() {
+//         Now how to convert the source to the authUser? That's why we are using MediatorLiveData
+        authUser.addSource(source, new Observer<AuthResource<User>>() {
             @Override
-            public void onChanged(User user) {
+            public void onChanged(AuthResource<User> user) {
                 authUser.setValue(user); // it will update any observers
                 authUser.removeSource(source); // Since we no longer need to listen to it
             }
         });
     }
 
-    public LiveData<User> observeUser() {
+    public MediatorLiveData<AuthResource<User>> observeUser() {
         return authUser;
     }
 }
